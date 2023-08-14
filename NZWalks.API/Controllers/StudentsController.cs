e7ff5c0cb98b765b40c712bdee25a9f7f985a127
Dean Chong <dchong@BORDET>
2023-08-14 23:19:42 +0800

    update

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+
+    // https://localhost:7107/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:7107/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "Daivd" };
+
+            return Ok(studentNames);
+        }
+    }
+}
